f68fdca2159801474f5976747ca2d664151a8eb8
CHOK <6206021611133@fitm.kmutnb.ac.th>
2022-09-25 12:26:45 +0700

    .

--- server.js
+++ server.js
@@ -87,8 +87,9 @@ app.get('/viewOrder', (services.orderView))
 app.use('/auth/', authRouter)
 app.use('/auth/product', productRouter)
 
+var port = process.env.PORT || 3000;
 // Step 9 - configure the server's port
-app.listen(3000, () => console.log('Server Started'))
+app.listen(port, '0.0.0.0', () => console.log('Server Started'))
 
 // Error handling
 app.use((err, req, res, next) => {
